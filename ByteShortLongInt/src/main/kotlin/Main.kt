30296e119e9c37f68ba18800eb1e90b154078ee6
Silvia Asmat <silvia.asmat@g.austincc.edu>
2023-06-18 11:00:22 -0500

    Intro to DataTypes

---
+++ ByteShortLongInt/src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main() {
+    var number: Int = 24
+
+    val maxIntegerValue: Int = Int.MAX_VALUE
+    val minIntegerValue: Int = Int.MIN_VALUE
+
+    println("Int maximum value is: $maxIntegerValue")
+    println("Int minimum value is: $minIntegerValue")
+
+    number = 2147483647
+
+    val myMaxByteValue: Byte = Byte.MAX_VALUE
+    val myMinByteValue: Byte = Byte.MIN_VALUE
+
+    println("Byte maximum value is: $myMaxByteValue")
+    println("Byte minimum value is: $myMinByteValue")
+
+    val myMaxShortValue: Short = Short.MAX_VALUE
+    val myMinShortValue: Short = Short.MIN_VALUE
+
+    println("Short maximum value is: $myMaxShortValue")
+    println("Short minimum value is: $myMinShortValue")
+
+    val myMaxLongValue: Long = Long.MAX_VALUE
+    val myMinLongValue: Long = Long.MIN_VALUE
+
+    println("Long maximum value is: $myMaxLongValue")
+    println("Long minimum value is: $myMinLongValue")
+
+    //Inferred data type is Int
+    val myNumber = 28
+}
