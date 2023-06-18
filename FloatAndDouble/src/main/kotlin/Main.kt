35240b1d6f2cc481665fd7fa3f4be44c77c92898
Silvia Asmat <silvia.asmat@g.austincc.edu>
2023-06-18 11:11:47 -0500

    Intro to Float and Double

---
+++ FloatAndDouble/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    //Infers type as float
+    val myNumber = 2.5f
+
+    //Infers type as double
+    val mySecondNumber = 2.5
+
+    println("myNumber = $myNumber and mySecondNumber = $mySecondNumber")
+}
