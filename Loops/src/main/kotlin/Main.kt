10e0f08ff21889923ba9a0eca7b1b9a39d73ff12
Silvia Asmat <silvia.asmat@g.austincc.edu>
2023-06-18 20:30:58 -0500

    Intro to FOR and WHILE Loops

---
+++ Loops/src/main/kotlin/Main.kt
@@ -0,0 +1,39 @@
+fun main() {
+
+// Demonstration of FOR loops
+//    for (i in 1..10) {
+//        println("i = $i")
+//    }
+
+//    for (i in 1 until 10) {
+//        println(i)
+//    }
+
+//    for (i in 1 until 10 step 2) {
+//        println(i)
+//    }
+
+//    for (i in 10 downTo 1 ) {
+//        println(i)
+//    }
+
+//    for (i in 1..10) {
+//        println(i)
+//    }
+
+//Demonstration of WHILE loops
+
+    var number = 0
+
+//    while (number < 10) {
+//        println(number)
+//        number++
+//    }
+
+//  while (number < 10) println(++number)
+
+//    do {
+//        println(number)
+//    } while (number < 10)
+
+}
